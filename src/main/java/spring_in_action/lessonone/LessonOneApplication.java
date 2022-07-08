db7c651bad6bfb344d896c2756e37c0a18c4df3d
Artamonov Vyacheslav <slava_artamonov96@mail.ru>
2022-07-08 21:12:50 +0500

    Первая глава

---
+++ src/main/java/spring_in_action/lessonone/LessonOneApplication.java
@@ -0,0 +1,12 @@
+package spring_in_action.lessonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonOneApplication.class, args);
+    }
+}
---
+++ src/test/java/spring_in_action/lessonone/LessonOneApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_in_action.lessonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
