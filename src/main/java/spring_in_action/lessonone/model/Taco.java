99f032fb2aa13360afd45329a8d95d91485ca266
Artamonov Vyacheslav <slava_artamonov96@mail.ru>
2022-07-08 21:53:51 +0500

    Добавил model для Taco и обработку запроса /design

---
+++ src/main/java/spring_in_action/lessonone/model/Taco.java
@@ -0,0 +1,12 @@
+package spring_in_action.lessonone.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+    private List<Ingredient> ingredients;
+}
